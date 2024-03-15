26c65126c73e5a29037da6b88f00cdca6761e123
Codewithdark <144595403+codewithdark-git@users.noreply.github.com>
2024-03-15 20:23:13 +0500

    Update cookies.py

--- cookies.py
+++ cookies.py
@@ -1,11 +1,11 @@
 from g4f.cookies import set_cookies
 
 set_cookies(".bing.com", {
-  "_U": "1eoegGAj-prTMbT_x8evELjNYN-1BRM4YqcTSPSymLn86arEV2vfXQtvex_HY7W4r4DX7zU2iGucEIcEWxhACKgGO3l8PuXMszy4VEPUCsvpTATVxgwjQ9sCqINMuXvcjhXAS9uQiJzsFAfSExlsevulo8dVMhfIMScw9f7PC_EN2hivWBPQ-OncXZzWsIz_055u9ahJz9jknbm7_e7V9ig"
+  "_clck": "w3y6fr%7C2%7Cfjl%7C0%7C1517"
 })
 set_cookies("chat.openai.com", {
-  "access_token": "2e21766412eed1bd0d34862b922322b6c25d7803e8369957ba0b0b019f5a0b1b%7C1b05fe52e2b0aef2e15232c39b411c711e0c9b68484c649959f4d46169689536"
+  "__cflb": "0H28vVfF4aAyg2hkHEuhVVUPGkAFmYvk57xoxRMRm1X"
 })
 set_cookies(".google.com", {
-  "__Secure-1PSID": "g.a000hQh_KYcwS81vIoGwzKsyOPzas0X1x5e-k5t1hP-yKs9Glr4Y2_LEXts4MwaXO0uqdK2RYwACgYKAfcSAQASFQHGX2MiwiuftYgVq5u-qdaAl6ibChoVAUF8yKrq4N1pGgtW7Yaf3M6tNAre0076"
+  "__Secure-3PSID": "g.a000hQh_KYcwS81vIoGwzKsyOPzas0X1x5e-k5t1hP-yKs9Glr4Y3QvqZySWierihtij8oAHvgACgYKAcgSAQASFQHGX2Mi2gQjmlI3XdU3m8xFFwtpzRoVAUF8yKoOoZXsIMZcs3KirlD37H8A0076"
 })
