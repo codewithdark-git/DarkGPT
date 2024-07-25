173b4317345c6bb8362653ca5a775cac9220164a
Dark Coder <codewithdark90@gmail.com>
2024-07-25 21:31:46 +0500

    updates

--- venv/Lib/site-packages/pyphen/dictionaries/update.sh
+++
@@ -1,7 +0,0 @@
-git clone git://anongit.freedesktop.org/libreoffice/dictionaries libreoffice-dictionaries
-cd libreoffice-dictionaries
-git pull
-cd ..
-find libreoffice-dictionaries -name "hyph_*\.dic" | xargs -I '{}' cp '{}' .
-sed -i 's/\r$//' *.dic
-rename -- -Latn _Latn *-Latn.dic
